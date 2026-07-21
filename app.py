0cc1021c7826eb577b99f4a6a05326503e93a794
niol8742 <niol8742@colorado.edu>
2026-07-21 15:36:34 +0000

    Added db_test route and added postgre support to Render route

--- app.py
+++ app.py
@@ -1,6 +1,40 @@
 from flask import Flask
+import psycopg2
+import os
 app = Flask(__name__)
+conn = None
+cur = None
+try:
+    conn = psycopg2.connect("postgresql://niol8742db_user:vC051v0HBBHJEroQn7HFFZhU4sR6whRO@dpg-d9fp1q3bc2fs73bl98qg-a/niol8742db")
+    cur = conn.cursor()
+
+    # SQL work goes here
+
+    conn.commit()
+    return "Success message here"
+except Exception as e:
+    if conn is not None:
+        conn.rollback()
+    return f"Database error: {e}"
+finally:
+    if cur is not None:
+        cur.close()
+    if conn is not None:
+        conn.close()
+
 
 @app.route("/")
 def index():
     return "Hello World from Nick OLeary in 3308"
+
+@app.route("/db_test")
+def db_test():
+    conn = None
+    try:
+        conn = psycopg2.connect("postgresql://niol8742db_user:vC051v0HBBHJEroQn7HFFZhU4sR6whRO@dpg-d9fp1q3bc2fs73bl98qg-a/niol8742db")
+        return "Database connection successful"
+    except Exception as e:
+        return f"Database connection failed: {e}"
+    finally:
+        if conn is not None:
+            conn.close()
